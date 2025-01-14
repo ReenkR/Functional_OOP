22fc3c0fd43aa4061965a5bbdaad084b64a80cda
ReenkR <rathodreenk.mitesh2023@vitstudent.ac.in>
2025-01-14 16:12:58 +0530

    Reenk first github commit

--- Abstraction/abstract_example.java
+++ Abstraction/abstract_example.java
@@ -2,9 +2,9 @@ package Abstraction;
 
 abstract class abstract_example 
 {
-	abstract void chris(); //abstract method
+	abstract void reenk(); //abstract method
 	
-	public void chris(String c) //concrete method
+	public void reenk(String c) //concrete method
 	{
 		System.out.println("This is a concrete method of abstract_example class"); //body of concrete method
 	}
